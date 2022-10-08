aa42a80eeb67e969972e9e96fc0c604a335aabc9
narpalan <thiagofumega@gmail.com>
2022-10-07 22:50:34 -0300

    requisito 6 e 7 concluidos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import SearchHome from './components/SearchHome';
 import ShoppingCart from './components/ShoppingCart';
+import ProductDetails from './components/ProductDetails';
 
 function App() {
   return (
@@ -16,6 +17,10 @@ function App() {
         <Route
           path="/shoppingcart"
           component={ ShoppingCart }
+        />
+        <Route
+          path="/details/:id"
+          render={ (props) => <ProductDetails { ...props } /> }
         />
       </Switch>
     </BrowserRouter>
--- src/services/api.js
+++ src/services/api.js
@@ -26,7 +26,7 @@ export async function getProductsFromCategoryAndQuery(categoryId, query) {
   return data;
 }
 
-export async function getProductById() {
-  // Esta implementação específica não é avaliada, mas pode ajudar você 🙂
-  // Atenção: essa função não deverá ser chamada na tela do carrinho de compras.
+export async function getProductById(productId) {
+  const result = await fetch(`https://api.mercadolibre.com/items/${productId}`);
+  return result.json();
 }
